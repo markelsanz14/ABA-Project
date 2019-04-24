bf281e2b0b8cc80b8282a9fc5b025953ce9624f4
ajemerson <ajemerson2013@gmail.com>
2019-04-24 15:29:37 -0400

    Randomizing train/test splits for NB baseline; implemented Iris dataset baseline

--- naive_bayes_analysis.R
+++ naive_bayes_analysis.R
@@ -19,19 +19,35 @@ X.letter.recognition <- letter.recognition[,2:17]
 Y.iris <- iris[,5]
 X.iris <- scale(iris[,1:4])
 
-### Predictions Using Standard NB:
+train.set.percentage <- 0.75
+set.seed(5) # Setting seed for reproducible results.
+
+### Predictions Using Standard NB as Baseline:
 
 # Letter Recogntion:
-Y.letter.recognition.train <- Y.letter.recognition[1:16000]
-X.letter.recognition.train <- X.letter.recognition[1:16000,]
-Y.letter.recognition.test <- Y.letter.recognition[16001:20000]
-X.letter.recognition.test <- X.letter.recognition[16001:20000,]
+letter.recognition.sample.size <- floor(train.set.percentage * length(Y.letter.recognition))
+letter.recognition.train.ind <- sample(seq_len(length(Y.letter.recognition)), size = letter.recognition.sample.size)
+
+Y.letter.recognition.train <- Y.letter.recognition[letter.recognition.train.ind]
+X.letter.recognition.train <- X.letter.recognition[letter.recognition.train.ind,]
+Y.letter.recognition.test <- Y.letter.recognition[-letter.recognition.train.ind]
+X.letter.recognition.test <- X.letter.recognition[-letter.recognition.train.ind,]
+
 nb.letter.recognition <- naive_bayes(X.letter.recognition.train, Y.letter.recognition.train)
 preds.letter.recognition <- predict(nb.letter.recognition, X.letter.recognition.test)
 letter.recognition.confusion.matrix <- confusionMatrix(preds.letter.recognition, Y.letter.recognition.test)
 
 # Iris:
-# TODO: Find different priors for the continuous covariates.
+iris.sample.size <- floor(train.set.percentage * length(Y.iris))
+iris.train.ind <- sample(seq_len(length(Y.iris)), size = iris.sample.size)
 
+Y.iris.train <- Y.iris[iris.train.ind]
+X.iris.train <- X.iris[iris.train.ind,]
+Y.iris.test <- Y.iris[-iris.train.ind]
+X.iris.test <- X.iris[-iris.train.ind,]
+
+nb.iris <- naive_bayes(X.iris.train, Y.iris.train)
+preds.iris <- predict(nb.iris, X.iris.test)
+iris.confusion.matrix <- confusionMatrix(preds.iris, Y.iris.test)
 
 
