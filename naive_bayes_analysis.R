1ddd6eaa359b8a2aa47547bbf74e318c3fe46724
ajemerson <ajemerson2013@gmail.com>
2019-04-23 22:11:04 -0400

    Downloading and preparing data (iris and letter recognition), loading NB library

--- naive_bayes_analysis.R
+++ naive_bayes_analysis.R
@@ -4,3 +4,16 @@
 # Main Goal: To explore the Naive Bayes classifier by comparing the "vanilla"
 # version as a baseline to an approach that calculates the full conditional distributions
 # of all covariates (removing conditional independence assumptions).
+
+library(naivebayes)
+library(rjags)
+
+# Separating data into response and covariates.
+
+# Letter Recognition (16 total covariates; discrete)
+Y.letter.recognition <- letter.recognition[,1]
+X.letter.recognition <- letter.recognition[,2:17]
+
+# Iris (4 total covariates; continuous)
+Y.iris <- iris[,5]
+X.iris <- scale(iris[,1:4])
