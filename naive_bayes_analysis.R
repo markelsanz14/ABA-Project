67030305ca64f2519329ebd6e62d91652e988f4e
ajemerson <ajemerson2013@gmail.com>
2019-04-23 21:41:38 -0400

    Creating base R file for all analysis

---
+++ naive_bayes_analysis.R
@@ -0,0 +1,6 @@
+# ST 540: Applied Bayesian Analysis
+# Authors: Andrew Emerson and Markel Sanz Ausin
+
+# Main Goal: To explore the Naive Bayes classifier by comparing the "vanilla"
+# version as a baseline to an approach that calculates the full conditional distributions
+# of all covariates (removing conditional independence assumptions).
