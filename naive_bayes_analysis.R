27406bc2803ebdf7a291e35d9ac9e81026764a12
ajemerson <ajemerson2013@gmail.com>
2019-04-24 13:24:45 -0400

    Creating simple NB baseline for Letter Recognition; will need to randomize train/test split

--- naive_bayes_analysis.R
+++ naive_bayes_analysis.R
@@ -7,6 +7,7 @@
 
 library(naivebayes)
 library(rjags)
+library(caret)
 
 # Separating data into response and covariates.
 
@@ -17,3 +18,20 @@ X.letter.recognition <- letter.recognition[,2:17]
 # Iris (4 total covariates; continuous)
 Y.iris <- iris[,5]
 X.iris <- scale(iris[,1:4])
+
+### Predictions Using Standard NB:
+
+# Letter Recogntion:
+Y.letter.recognition.train <- Y.letter.recognition[1:16000]
+X.letter.recognition.train <- X.letter.recognition[1:16000,]
+Y.letter.recognition.test <- Y.letter.recognition[16001:20000]
+X.letter.recognition.test <- X.letter.recognition[16001:20000,]
+nb.letter.recognition <- naive_bayes(X.letter.recognition.train, Y.letter.recognition.train)
+preds.letter.recognition <- predict(nb.letter.recognition, X.letter.recognition.test)
+letter.recognition.confusion.matrix <- confusionMatrix(preds.letter.recognition, Y.letter.recognition.test)
+
+# Iris:
+# TODO: Find different priors for the continuous covariates.
+
+
+
